2c1cb01058b6f0cd3dbfd907aec7053831ca6765
amine-zergan <amine.mejri1000@gmail.com>
2022-11-15 15:00:29 +0100

    first upload

---
+++ lib/formwidget.dart
@@ -0,0 +1,8 @@
+library formwidget;
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
+part 'src/form.dart';
+part 'src/user_form.dart';
+part 'src/password_form.dart';
